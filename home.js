a1b052694881b4a7e426c1cd6c3c6442a695cda9
Roberta <robertalee2006@gmail.com>
2025-05-27 01:18:45 -0700

    Contact button and add to cart buttons

--- home.js
+++ home.js
@@ -4,8 +4,13 @@ const shopButton = document.querySelector(`.shop-button`);
 
 const contactButton = document.querySelector(`.contact-button`);
 
+const homeAssets = document.querySelector(`.home-assets`);
+
+const contactCard = document.querySelector(`#contact-card`);
 const waveSound = document.querySelector(`#wave-sound`);
 const homeLogo = document.querySelector(`.big-logo img`);
+
+contactCard.classList.add(`hidden`);
 
 homeLogo.addEventListener(`click`, () => {
   waveSound.play();
@@ -13,4 +18,18 @@ homeLogo.addEventListener(`click`, () => {
 
 shopButton.addEventListener(`click`, () => {
   window.location.href = "top.html";
+});
+
+contactButton.addEventListener(`click`, () => {
+  console.log(`contact  button clicked`);
+  contactCard.classList.toggle(`hidden`);
+  homeAssets.classList.toggle(`.blur`);
+});
+
+contactCard.classList.add("fade-out");
+
+contactCard.addEventListener(`click`, () => {
+  console.log(`contact  button clicked`);
+  contactCard.classList.add("hidden");
+  contactCard.classList.remove("fade-out");
 });
